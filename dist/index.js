bbef8901c625aaeea405bf9300714a8f371b8bce
Fletcher Lind <fletcher.lind.seagull@gmail.com>
2025-03-31 23:56:47 +0200

    Initial v2.0 (dist)

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./commands");
-const program_1 = require("./program");
-program_1.program.parse(process.argv);
